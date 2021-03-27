a2a5b9e9cf1ced44d1c3aa089915262c880c6cf8
ufonetcom <ufozx@naver.com>
2021-03-27 19:59:34 +0900

    <inflearn>테스트코드 작성

--- src/main/java/me/doompok/inflearnthejavatest/Study.java
+++ src/main/java/me/doompok/inflearnthejavatest/Study.java
@@ -1,4 +1,13 @@
 package me.doompok.inflearnthejavatest;
 
 public class Study {
+    private int limit;
+
+    public Study(int limit) {
+        this.limit = limit;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
 }
--- src/test/java/me/doompok/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/doompok/inflearnthejavatest/StudyTest.java
@@ -7,14 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudyTest {
 
     @Test
+    @DisplayName("스터디 만들기 111")
     void create() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+        Study study = new Study(-10);
+
+        //assertAll을 안쓰면 한메소드에 해당 테스트라인이 실패하면 그밑으로는 진행이 되지 않는다
+        //그래서 assertAll로 묶어주면 이 안에 테스트라인들이 실패/성공 모두 실행된다.
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertTrue(study.getLimit() > 0, "스터디 참석인원 0보다 커야한다.")
+        );
+
     }
 
+//    @Disabled  //해당 메서드 테스트 실행시키지 않기
     @Test
-    @Disabled  //해당 메서드 테스트 실행시키지 않기
+    @DisplayName("스터디 만들기\uD83C\uDF54 ")
     void create1() {
         System.out.println("create1");
     }
