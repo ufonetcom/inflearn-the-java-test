d6948b4c63662e4be9ab0fc120914869127681f1
ufonetcom <ufozx@naver.com>
2021-03-30 16:39:41 +0900

    <inflearn 강의> TestCode

--- src/test/java/me/doompok/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/doompok/inflearnthejavatest/StudyTest.java
@@ -1,15 +1,28 @@
 package me.doompok.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//기본 테스트의 인스턴스 작동 원리는 하나의 메소드마나 각기다른 하나의 인스턴스를 생성
+//각 메소드의 사용돼는 인스턴스를 하나의 인스턴스로 쓰게 해주는 기능
+//BeforeAll과 AfterAll의 static은 생략 가능
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudyTest {
 
+    int value = 1;
+
+    @Order(2)
     @Test
     @DisplayName("스터디 만들기 111")
     void create() {
-        Study study = new Study(-10);
+        System.out.println(this);
+        System.out.println(value++);
+
+        Study study = new Study(10);
 
         //assertAll을 안쓰면 한메소드에 해당 테스트라인이 실패하면 그밑으로는 진행이 되지 않는다
         //그래서 assertAll로 묶어주면 이 안에 테스트라인들이 실패/성공 모두 실행된다.
@@ -21,10 +34,29 @@ class StudyTest {
     }
 
 //    @Disabled  //해당 메서드 테스트 실행시키지 않기
+    @Order(1)
     @Test
-    @DisplayName("스터디 만들기\uD83C\uDF54 ")
+    @DisplayName("스터디 만들기222")
     void create1() {
+        System.out.println(this);
+        System.out.println(value);
         System.out.println("create1");
+    }
+
+    @Order(3)
+    @DisplayName("스터디 반복하기111")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test "+repetitionInfo.getCurrentRepetition() +"/"+
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @Order(4)
+    @DisplayName("스터디 반복하기222")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "더워지고", "있넫"})
+    void parameterrizedTest(String message) {
+        System.out.println(message);
     }
 
     @BeforeAll
